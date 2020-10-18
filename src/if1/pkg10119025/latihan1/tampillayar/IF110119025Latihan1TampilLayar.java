7bbfb04c029b93c23d361b567ba0b4f91ed20d53
unknown <adhiragathan12@gmail.com>
2020-10-18 13:01:29 +0700

    initial commit

---
+++ src/if1/pkg10119025/latihan1/tampillayar/IF110119025Latihan1TampilLayar.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119025.latihan1.tampillayar;
+
+/**
+ *
+ * @author User
+ */
+public class IF110119025Latihan1TampilLayar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "------------------");
+        System.out.println("NAMA\t\t: Adhira Fahri Gathan");
+        System.out.println("ALAMAT\t\t: Jalan Hayati Mahim no 14");
+        System.out.println("TEMPAT LAHIR\t: Tanjungpandan");
+        System.out.println("TANGGAL LAHIR\t: 01 Desember 2001");
+        System.out.println("------------------");
+        System.out.println("HOBI\t\t: Olahraga & Nonton");
+        System.out.println("MAKES\t\t: Nasi Goreng ");
+        System.out.println("MIKES\t\t: Lemon Tea");
+                
+    }
+    
+}
